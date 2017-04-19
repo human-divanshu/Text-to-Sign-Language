12fcab0b3bcdd2e4b50fb296bbfa4ca6496e2c46
Divanshu Singh <human.divanshu@gmail.com>
2017-04-20 01:07:21 +0530

    Removed bug

--- admin/core/database.php
+++ admin/core/database.php
@@ -3,7 +3,6 @@
 // connect to database now this can used anywhere
 
 function connect() {
-    //$db = new PDO('mysql:host=localhost;dbname=bestifdf_text2sign;charset=utf8', 'bestifdf_isl', 'thapar@123');
     $db = new PDO('mysql:host=localhost;dbname=text2sign;charset=utf8', 'root', 'root');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
